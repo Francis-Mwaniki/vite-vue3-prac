9e751dcf3ca0d220eefb974cdde30471a070fb2c
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-10-21 17:36:06 +0300

    vite speed

---
+++ dashboard/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div></div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
